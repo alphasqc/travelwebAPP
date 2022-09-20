9fdb8f6fb257d2385c355bafaf2c7d4354a08489
js179000250 <9736674+js179000250@user.noreply.gitee.com>
2022-09-20 17:59:20 +0800

    navbar的更改

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,21 +2,42 @@
   <div class="home">
     <van-nav-bar>
       <template #left>
-        <van-icon name="home-o" size="18" />
+        <van-cell is-link @click="showPopup">
+          <van-icon name="user-o" size="18" />
+        </van-cell>
+        <van-popup v-model:show="show" round closeable position="left" :style="{ height: '100%', width: '50%' }" />
+      </template>
+      <template #title>
+        <van-search @click-input="jumpsearch" shape="round" placeholder="请输入搜索关键词" input-align="center" />
       </template>
       <template #right>
-        <van-icon name="search" size="18" />
+        <van-icon name="notification-f" class-prefix="iconfont" size="18" />
       </template>
     </van-nav-bar>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
 
 export default {
-  name: 'HomeView',
-  components: {
+  setup () {
+    // 左侧弹出
+    const show = ref(false)
+    const showPopup = () => {
+      show.value = true
+    }
+    // 搜索跳转
+    const router = useRouter()
+    const jumpsearch = () => {
+      router.push('/wallet')
+    }
+    return {
+      show,
+      jumpsearch,
+      showPopup
+    }
   }
 }
 </script>
