d8446e14be7607fea4b1080a6a30a7f6a8d219d4
js179000250 <9736674+js179000250@user.noreply.gitee.com>
2022-09-21 23:23:30 +0800

    jsonsever数据和轮播

--- src/main.js
+++ src/main.js
@@ -3,14 +3,24 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 // 1. 引入你需要的组件
-import { Search, Popup, Tabbar, TabbarItem, Button, NavBar, Icon } from 'vant'
+import { Swipe, SwipeItem, Search, Popup, Tabbar, TabbarItem, Button, NavBar, Icon, Cell } from 'vant'
 // 2. 引入组件样式
 import 'vant/lib/index.css'
 import '@/assets/iconfont/iconfont.css'
 
+import { http } from '@/axios/api'
+import '@/mock/index'
+
 const app = createApp(App)
+
+app.config.globalProperties.$axios = http
+
 app.use(store)
 app.use(router)
+
+app.use(Swipe)
+app.use(SwipeItem)
+app.use(Cell)
 app.use(Tabbar)
 app.use(TabbarItem)
 app.use(Button)
@@ -18,4 +28,5 @@ app.use(NavBar)
 app.use(Icon)
 app.use(Popup)
 app.use(Search)
+
 app.mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    proxy: {
+      '/api': {
+        // target: 'https://958af609-1601-4c7f-87e9-51b368ae6645.mock.pstmn.io',
+        target: 'http://localhost:3000/',
+        pathRewrite: {
+          '^/api': '/'
+        }
+      }
+    }
+  }
 })
