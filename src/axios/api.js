4255534f96a384b9f019a921ff59b76fec1a60fc
js179000250 <9736674+js179000250@user.noreply.gitee.com>
2022-09-22 09:59:54 +0800

    jsonsever与轮播使用

--- src/axios/api.js
+++ src/axios/api.js
@@ -1,5 +1,15 @@
 import axios from 'axios'
 
-export const http = axios.create({
-  baseURL: 'http://localhost:3000/'
-})
+// export const http = axios.create({
+//   baseURL: 'http://localhost:3000/'
+// })
+
+const baseURL = 'http://localhost:3000/'
+
+export function getHome (url) {
+  return axios.get(`${baseURL}${url}`)
+}
+
+export default {
+  getHome
+}
